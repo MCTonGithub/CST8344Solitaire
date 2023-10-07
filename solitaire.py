cedec7535ee74280de57bc2c5cdad6db1611415f
Jian Zhou <zhou0214@algonquinlive.com>
2023-10-07 12:13:54 -0400

    check card sprite overlapping with pile sprite

--- solitaire.py
+++ solitaire.py
@@ -82,6 +82,10 @@ class Solitaire(arcade.Window):
         # Sprite list with all the mats tha cards lay on.
         self.pile_mat_list: arcade.SpriteList = arcade.SpriteList()
 
+        # Sprite list with all piles for dropping cards.
+
+        self.dropping_pile_list: arcade.SpriteList = arcade.SpriteList()
+
         # This is the location for the talon and the stock
         # pile #0 is stock pile
         # pile #1 is talon
@@ -106,6 +110,8 @@ class Solitaire(arcade.Window):
 
         self.shuffle_cards()  # Shuffle the cards when setting up the game.
 
+
+
     def create_stock_talon(self):
         pile = arcade.SpriteSolidColor(MAT_WIDTH, MAT_HEIGHT, arcade.csscolor.DARK_SEA_GREEN)
         pile.position = LEFT_X, TOP_Y
@@ -123,12 +129,14 @@ class Solitaire(arcade.Window):
             pile = arcade.SpriteSolidColor(MAT_WIDTH, MAT_HEIGHT, arcade.csscolor.DARK_SEA_GREEN)
             pile.position = MIDDLE_X + i * X_SPACING, MIDDLE_Y
             self.pile_mat_list.append(pile)
+            self.dropping_pile_list.append(pile)
 
     def create_foundation(self):
         for i in range(4):
             pile = arcade.SpriteSolidColor(MAT_WIDTH, MAT_HEIGHT, arcade.csscolor.DARK_SEA_GREEN)
             pile.position = RIGHT_X - i * X_SPACING, TOP_Y
             self.pile_mat_list.append(pile)
+            self.dropping_pile_list.append(pile)
 
     def shuffle_cards(self):
         """ Shuffle the card list. """
@@ -198,9 +206,83 @@ class Solitaire(arcade.Window):
         if len(self.held_cards) == 0:
             return
 
+        # check if the card is dropped on a mat
+        # get the top card on the mat
+
+
+        # Check if there is an overlapping sprite with the released sprite
+
+        #right now, it only check piles. in the future, it should check the top cards of the piles.
+        overlapping_sprite = self.get_overlapping_sprite(self.held_cards[0], self.dropping_pile_list)
+
+        if overlapping_sprite:
+            print(f"Overlapping sprite found: {overlapping_sprite}")
+        else:
+            print("No overlapping sprite found")
+
         # shows that no card is held
         self.held_cards = []
 
+    # checks if the card is in the mat, or card on top of another card
+    def check_overlap(self, sprite1, sprite2):
+        """
+        Check if two sprites overlap with an overlap ratio greater than 50%.
+
+        Args:
+            sprite1 (arcade.Sprite): The first sprite.
+            sprite2 (arcade.Sprite): The second sprite.
+
+        Returns:
+            bool: True if the overlap ratio is greater than 50%, False otherwise.
+        """
+        # Get the bounding box of the first sprite
+        left1 = sprite1.left
+        right1 = sprite1.right
+        top1 = sprite1.top
+        bottom1 = sprite1.bottom
+
+        # Get the bounding box of the second sprite
+        left2 = sprite2.left
+        right2 = sprite2.right
+        top2 = sprite2.top
+        bottom2 = sprite2.bottom
+
+        # Calculate the overlap area
+        overlap_width = max(0, min(right1, right2) - max(left1, left2))
+        overlap_height = max(0, min(top1, top2) - max(bottom1, bottom2))
+        overlap_area = overlap_width * overlap_height
+
+        # Calculate the area of the smaller sprite
+        min_area = min(sprite1.width * sprite1.height, sprite2.width * sprite2.height)
+
+        # Calculate the overlap ratio
+        overlap_ratio = overlap_area / min_area
+
+        # Check if overlap ratio is greater than 0.5 (50%)
+        if overlap_ratio > 0.5:
+            return True
+
+        return False
+
+
+    # gets the overlapping sprite (assumming that only one sprite is overlapping over 50%)
+    def get_overlapping_sprite(self, sprite1, sprite_list):
+        """
+        Get the sprite from sprite_list that overlaps with sprite1, if one exists.
+
+        Args:
+            sprite1 (arcade.Sprite): The sprite to check for overlap.
+            sprite_list (arcade.SpriteList): The list of sprites to check against.
+
+        Returns:
+            arcade.Sprite or None: The overlapping sprite, or None if no overlap is found.
+        """
+        for sprite2 in sprite_list:
+            if sprite2 != sprite1:
+                if self.check_overlap(sprite1, sprite2):
+                    return sprite2  # Return the overlapping sprite if found
+
+        return None  # Return None if no overlap is found
 
 def main():
     """ Main function """
