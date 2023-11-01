d8ccb5c2ae2d94a0b929b19b5ad787ca6a3523d8
Matthew Tina <matthewtina2002@gmail.com>
2023-11-01 02:59:45 -0400

    Trivial. Added back the main function to solitaire.py

--- solitaire.py
+++ solitaire.py
@@ -484,3 +484,12 @@ class Solitaire(arcade.Window):
 
 
 
+def main():
+    """ Main function """
+    window = Solitaire()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
